fb0a42bbe9ddba0d1e05b56c65d5278cc0a7c9f1
KimJeeHun1 <iopuioyui@naver.com>
2026-05-08 09:49:55 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -5,13 +5,23 @@ struct student{
   int score;
   struct* student;
 }
+void add(char* name, int score, student* s){
+  if(s==NULL){
+    s->name=name;
+    s->score=score;
+    return 0;
+  }
+  s->student.name = name;
+  s->student.score = score;
+}
+void delete(char * name){
+  
+}
 
-void linked_list(){
+
+int main(){
+  
   while(1){
     
   }
-}
-
-int main(){
-  linked_list();
 }
