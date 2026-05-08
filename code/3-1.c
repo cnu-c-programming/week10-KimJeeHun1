4705a6cc6ff3aca9861252e10e6b7a113b290ab6
KimJeeHun1 <iopuioyui@naver.com>
2026-05-08 10:40:37 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+int student_number = 0;
 
-struct student{
+struct Student{
   char name[100];
   int score;
   struct student s;
 }
-void add(char* name, int score, student* s){
-  if(s==NULL){
-    s->name=name;
-    s->score=score;
-    return 0;
-  }
-  s->s.name = name;
-  s->s.score = score;
+Student* head=NULL;
+
+void add(char* name, int score){
+  
+  student_number++;
 }
 void delete(char * name){
   
 }
-
+void print(){
+  
+}
 
 int main(){
   
